3e68a35e7b238177d69047aa97f7f6bea7e78d9f
maitredjims <darel.djimbi@hotmail.fr>
2015-04-29 12:02:47 +0200

    ajout méthodes update et delete dans l'interface Contact

--- module/AddressBook/src/AddressBook/Service/Contact/ContactServiceInterface.php
+++ module/AddressBook/src/AddressBook/Service/Contact/ContactServiceInterface.php
@@ -5,11 +5,14 @@ namespace AddressBook\Service\Contact;
 interface ContactServiceInterface 
 {
     public function getAll();
-    
+
     public function getById($id);
-    
+
     public function getByIdWithSociete($id);
-    
+
     public function insert(\Zend\Form\Form $form, $dataAssoc);
-    
+
+    public function update($id, \Zend\Form\Form $form, $dataAssoc);
+
+    public function delete($id);
 }
