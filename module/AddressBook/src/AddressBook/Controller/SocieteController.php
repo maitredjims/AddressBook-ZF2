d8520708235666a81d5b3c05da97e278253ea8d2
maitredjims <darel.djimbi@hotmail.fr>
2015-04-30 10:40:32 +0200

    ajout updateAction et deleteAction

--- module/AddressBook/src/AddressBook/Controller/SocieteController.php
+++ module/AddressBook/src/AddressBook/Controller/SocieteController.php
@@ -8,69 +8,115 @@ use Zend\Http\Request;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class SocieteController extends AbstractActionController
-{
+class SocieteController extends AbstractActionController {
+
     /**
      *
      * @var Request
      */
     protected $request;
-    
+
     /**
      *
      * @var SocieteServiceInterface
      */
     protected $societeService;
-    
+
     public function __construct(SocieteServiceInterface $societeService) {
         $this->societeService = $societeService;
     }
-    
+
     public function listAction() {
         $listeSocietes = $this->societeService->getAll();
-        
+
         return new ViewModel(array(
             'societes' => $listeSocietes
         ));
     }
-    
-     public function showAction()
-    {
-        //$service = $this->getContactService();
-        
+
+    public function showAction() {
+
         $id = $this->params('id');
-        
-        //$contact = $service->getById($id);
+
         $societe = $this->societeService->getById($id);
-        
-        if(!$societe) {
+
+        if (!$societe) {
             // Génération d'une réponse d'erreur
             return $this->notFoundAction();
         }
-        
+
         return new ViewModel(array(
             'societe' => $societe
         ));
     }
-    
-    public function addAction()
-    {
-        $form = new SocieteForm();        
-        
-        if($this->request->isPost()) {
-            
+
+    public function addAction() {
+        $form = new SocieteForm();
+
+        if ($this->request->isPost()) {
+
             $contact = $this->societeService->insert($form, $this->request->getPost());
-            
-            if($contact) {
+
+            if ($contact) {
                 $this->flashMessenger()->addSuccessMessage('La société a bien été inséré');
-            
+
                 return $this->redirect()->toRoute('societe');
             }
-            
         }
-        
+
         return new ViewModel(array(
             'form' => $form
         ));
     }
+
+    public function updateAction() {
+        $id = (int) $this->params('id');
+
+        if (!$id) {
+            return $this->redirect()->toRoute('societe');
+        }
+
+        $societe = $this->societeService->getById($id);
+
+        $form = new SocieteForm();
+        $form->bind($societe);
+
+        if ($this->request->isPost()) {
+            $societe_update = $this->societeService->update($id, $form, $this->request->getPost());
+
+            if ($societe_update) {
+                return $this->redirect()->toRoute('societe');
+            }
+        }
+
+        return new ViewModel(array(
+            'form' => $form,
+        ));
+    }
+
+    public function deleteAction() {
+        $id = (int) $this->params('id');
+
+        if (!$id) {
+            return $this->redirect()->toRoute('societe');
+        }
+
+        if ($this->request->isPost()) {
+            $del = $this->request->getPost('del', 'Non');
+
+            if ($del == 'Oui') {
+                $id = (int) $this->request->getPost('id');
+                $this->societeService->delete($id);
+            }
+
+            // Redirect to list of contact
+            return $this->redirect()->toRoute('societe');
+        }
+
+        return array(
+            'id' => $id,
+            'societe' => $this->societeService->getById($id)
+        );
+    }
+
 }
